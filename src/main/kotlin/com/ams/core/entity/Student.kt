96dab712042b736e6a3325d1297d786b3c740ce8
jimmyberg <jimmyberg.kim@gmail.com>
2022-01-13 21:47:43 +0900

    annotation 정리

--- src/main/kotlin/com/ams/core/entity/Student.kt
+++ src/main/kotlin/com/ams/core/entity/Student.kt
@@ -3,7 +3,6 @@ package com.ams.core.entity
 import com.ams.core.common.enum.GenderEnum
 import com.ams.core.common.enum.StatusEnum
 import org.springframework.data.annotation.Id
-import org.springframework.data.annotation.Transient
 import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Table
 import reactor.core.publisher.Mono
